8cf4aad8515724b7c409b57f9a11b07ce5cecca6
Damon Kelley <damon.kelley@gmail.com>
2015-04-03 16:48:22 -0500

    Setup configurations.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+import os
 import re
+from setuptools import setup, find_packages
 
 version = ''
 with open('pynaco/__init__.py', 'r') as fd:
@@ -10,15 +12,23 @@ tests_require = [
     'ddt >= 1.0.0'
 ]
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
 setup(
     name='pynaco',
     version=version,
-    description='Python implementation of the NACO normalization rules',
+    description='Python implementation of the NACO normalization rules.',
+    long_description=README,
     url='https://github.com/unt-libraries/pynaco',
-    packages=find_packages(exclude=['tests.*', 'tests']),
-    package_data={'': ['LICENSE']},
+    packages=find_packages(),
     include_package_data=True,
     license='',
     tests_require=tests_require,
     test_suite='runtests',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    )
 )
