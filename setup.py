823a6163895e077282a2243095b373ebeaaf8151
Damon Kelley <damon.kelley@gmail.com>
2015-04-03 16:51:17 -0500

    Use context to open and read from the readme file.

--- setup.py
+++ setup.py
@@ -8,17 +8,19 @@ with open('pynaco/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
+readme = ''
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    readme = f.read()
+
 tests_require = [
     'ddt >= 1.0.0'
 ]
-
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
     name='pynaco',
     version=version,
     description='Python implementation of the NACO normalization rules.',
-    long_description=README,
+    long_description=readme,
     url='https://github.com/unt-libraries/pynaco',
     packages=find_packages(),
     include_package_data=True,
