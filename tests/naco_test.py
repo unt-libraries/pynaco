8408d939cc8f0a7acfef523cd95e9d3d6c2b561a
Damon Kelley <damon.kelley@gmail.com>
2015-04-03 11:32:30 -0500

    Add tests utilizing the .script and .check files.

--- tests/naco_test.py
+++ tests/naco_test.py
@@ -1,10 +1,48 @@
+import os
 import unittest
+
 from pynaco import naco
+from ddt import ddt, data, unpack
+
+# Absolute path to the data directory
+DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 
 
+def standard_data():
+    """Data provider method for the normalize function."""
+    script = open(os.path.join(DATA_PATH, 'NACO.script'))
+    check = open(os.path.join(DATA_PATH, 'NACOstandard.check'))
+
+    # Create a list of tuples in the form of
+    # (unormalized data, expected output)
+    standard = zip(script.readlines(), check.read().splitlines())
+
+    script.close()
+    check.close()
+
+    return standard
+
+
+def simplified_data():
+    """Data provider method for the normalizeSimplified function."""
+    script = open(os.path.join(DATA_PATH, 'NACO.script'))
+    check = open(os.path.join(DATA_PATH, 'NACOsimplified.check'))
+
+    # Create a list of tuples in the form of
+    # (unormalized data, expected output)
+    simplified = zip(script.readlines(), check.read().splitlines())
+
+    script.close()
+    check.close()
+
+    return simplified
+
+
+@ddt
 class NACOTest(unittest.TestCase):
 
     def test_capitalization_first_letter(self):
+        print DATA_PATH
         self.assertEqual('title', naco.normalize("Title", True))
 
     def test_capitalization_last_letter(self):
@@ -40,6 +78,16 @@ class NACOTest(unittest.TestCase):
     def test_leading_and_commas_retain(self):
         self.assertEqual(', a a',
                          naco.normalize("'\x1fa,,A,A'", 1))
+
+    @unpack
+    @data(*standard_data())
+    def test_normalize(self, value, expected):
+        self.assertEqual(expected, naco.normalize(value, True))
+
+    @unpack
+    @data(*simplified_data())
+    def test_normalizeSimplified(self, value, expected):
+        self.assertEqual(expected, naco.normalizeSimplified(value))
 
 
 def suite():
